1ec41391a14940f25f0da41d47428e85745e04f6
Ki Fung Leung <kifungleung19970611@gmail.com>
2022-12-07 00:56:58 +0800

    Software Overview !

--- motor_control.py
+++ motor_control.py
@@ -1,3 +1,7 @@
+# This is an automated data logging system of STM32 Motor Controller
+# Users can make use of STM32 Motor Control Protocol to retrieve the motors' parameters & status to work on the evalution
+# For more details, users are required to read the STM32MCP SDK Datasheet
+
 import matplotlib
 import tkinter as tk
 import threading
