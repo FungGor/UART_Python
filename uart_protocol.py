a8e9f663a975a593c0fa0d31aced35a2c588b478
FungGor <kifungleung19970611@gmail.com>
2022-12-06 13:10:30 +0800

    uart has been added

--- uart_protocol.py
+++ uart_protocol.py
@@ -1,1 +1,16 @@
+# https://www.engineersgarage.com/articles-raspberry-pi-serial-communication-uart-protocol-ttl-port-usb-serial-boards/
 import serial
+
+class uart_init():
+     def __init__(self, baudrate, parity, stopbits, bytesize, timeout):
+        self.baudrate = baudrate
+        self.parity = parity
+        self.stopbits = stopbits
+        self.bytesize = bytesize
+        self.timeout = timeout
+     
+     def uart_Open(self):
+        ser = serial.Serial("COM8",baudrate = self.baudrate, parity = self.parity, 
+        stopbits = self.stopbits, 
+        bytesize = self.bytesize,
+        timeout = self.timeout)
