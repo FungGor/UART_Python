02072e8d0c7fd9ea696ccfa647d4c7d5a550d940
Ki Fung Leung <kifungleung19970611@gmail.com>
2022-12-31 01:31:01 +0800

    uart modified

--- STM32MCP_CTL.py
+++ STM32MCP_CTL.py
@@ -1,7 +1,8 @@
 #This library implements the functions for controllling the STM32 Motor Driver (Applicable for F1xxx and F4xxx Series) 
 import STM32MCP_Lib
 
-#define UART Configuration Parameters
+#define UART Configuration Parameters 
+#Create UART Objects
 UART_PORT  = "COM3"
 BAUD_RATE  = 9600
 PARITY     = 'NONE'
@@ -9,7 +10,6 @@ STOPBITS   = 1
 BYTE_SIZE  = 8
 TIMEOUT    = None
 uart_Event = None
-
 uart = STM32MCP_Lib.STM32MCP_uartManager_t(UART_PORT,BAUD_RATE,PARITY,STOPBITS,BYTE_SIZE,TIMEOUT,uart_Event)
 
 
